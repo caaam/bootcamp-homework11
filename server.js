7df2e5919e6ae9315f6a361276f24b6301011a0f
cam <cam@camherbert.com>
2022-01-16 14:55:57 -0500

    fixed port for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const dbData = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
